e51cdef238c0f4988c62478d5524a064bb0aa785
Barrett Golding <bgMBP@mu-152251.dhcp.missouri.edu>
2016-02-23 08:49:47 -0600

    Add uninstall functions (not called yet).

--- uninstall.php
+++ uninstall.php
@@ -3,29 +3,78 @@
 /**
  * Fired when the plugin is uninstalled.
  *
- * When populating this file, consider the following flow
- * of control:
- *
- * - This method should be static
- * - Check if the $_REQUEST content actually is the plugin name
- * - Run an admin referrer check to make sure it goes through authentication
- * - Verify the output of $_GET makes sense
- * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
- *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
  * @link       http://example.com
  * @since      1.0.0
  *
- * @package    Plugin_Name
+ * @package    Postscript
  */
 
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
+}
+
+/**
+ * Removes plugin's data.
+ *
+ * @since 1.0.0
+ */
+function postscript_remove_plugin_data() {
+    $option    = 'postscript';
+    $meta_key  = 'postscript_meta';
+    $tax_styles = 'postscript_styles';
+    $tax_scripts = 'postscript_scripts';
+
+    postscript_remove_meta( $meta_key );
+    postscript_remove_tax_terms( $tax_styles );
+    postscript_remove_tax_terms( $tax_scripts );
+    postscript_remove_option( $option );
+}
+
+/**
+ * Removes plugin's option from database.
+ *
+ * @since 1.0.0
+ */
+function postscript_remove_option( $option ) {
+    delete_option( $option_name );
+}
+
+/**
+ * Removes plugin's post meta from database.
+ *
+ * @since 1.0.0
+ */
+function postscript_remove_meta( $meta_key ) {
+    delete_post_meta_by_key ( $meta_key );
+}
+
+/**
+ * Removes plugin's custom taxonomy terms from database.
+ *
+ * @since 1.0.0
+ */
+function postscript_remove_tax_terms( $tax ) {
+    $terms = get_terms( $tax );
+
+    foreach ( $terms as $term ) {
+        wp_delete_term( $term->term_id, $tax );
+    }
+
+    postscript_unregister_tax( $tax );
+}
+
+/**
+ * Unregisters plugin's custom taxonomy.
+ *
+ * @since 1.0.0
+ */
+function postscript_unregister_tax( $tax ) {
+    global $wp_taxonomies;
+    if ( taxonomy_exists( $tax ) ) {
+        unset( $wp_taxonomies[ $tax ] );
+    }
+
+    return;
 }
