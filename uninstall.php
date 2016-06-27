394b36e536fc42f4fa686616de70fb9ff4612cea
Barrett Golding <bgMBP@BG-MBP.local>
2016-06-27 02:43:14 -0600

    Correct uninstall inline comment.

--- uninstall.php
+++ uninstall.php
@@ -17,7 +17,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 /**
  * Register taxonomies (to get and delete its terms).
  *
- * Plugin now deactivated so tax registration no longer exists.
+ * Plugin not activated so tax registration no longer exists.
  *
  * @since   0.1.0
  */
